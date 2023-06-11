8e6f18a617b69674026b0eb0453f5f53016d78f5
Maxim <334890@gmail.com>
2023-06-11 16:37:52 +0600

    implement buildTree method

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -1,7 +1,10 @@
 export default function Tree(array = undefined) {
-  const sortedArray = array ? [...array].sort((a, b) => a - b) : undefined;
-  //todo: check for duplicates
+  const sortedArray = array
+    ? removeDuplicates([...array].sort((a, b) => a - b))
+    : undefined;
+
   let root = buildTree(sortedArray);
+  //prettyPrint(root);
 
   function buildTree(array) {
     if (!array || array.length === 0) {
@@ -11,15 +14,41 @@ export default function Tree(array = undefined) {
     const midElement = array[midIndex];
     const leftSubarray = array.slice(0, midIndex);
     const rightSubarray = array.slice(midIndex + 1);
-    console.table([
-      { array, midIndex, midElement, leftSubarray, rightSubarray },
-    ]);
-    return {};
+    //console.log({ array, midIndex, midElement, leftSubarray, rightSubarray });
+    return Node(midElement, buildTree(leftSubarray), buildTree(rightSubarray));
   }
 
-  function Node(value) {
-    return { value, left: null, right: null };
+  function Node(data = undefined, left = null, right = null) {
+    if (data === undefined) {
+      return null;
+    }
+    return { data, left, right };
   }
 
-  return {};
+  function removeDuplicates(sortedArray) {
+    let previousElement = null;
+    const deduplicatedArray = [];
+    for (let element of sortedArray) {
+      if (element != previousElement) {
+        deduplicatedArray.push(element);
+        previousElement = element;
+      }
+    }
+    return deduplicatedArray;
+  }
+
+  function prettyPrint(node = root, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  }
+
+  return { root, prettyPrint };
 }
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import Tree from "./BinarySearchTree.js";
 
-Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+const myTree = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 77]);
+myTree.prettyPrint();
