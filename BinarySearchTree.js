31abd54857fb35b79e8dcfb91b0a922b7dd08295
Maxim <334890@gmail.com>
2023-06-11 16:08:05 +0600

    create html file

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -1,9 +1,25 @@
-export default function Tree(array) {
-  let root = null;
+export default function Tree(array = undefined) {
+  const sortedArray = array ? [...array].sort((a, b) => a - b) : undefined;
+  //todo: check for duplicates
+  let root = buildTree(sortedArray);
 
-  return {};
+  function buildTree(array) {
+    if (!array || array.length === 0) {
+      return null;
+    }
+    const midIndex = Math.floor(array.length / 2);
+    const midElement = array[midIndex];
+    const leftSubarray = array.slice(0, midIndex);
+    const rightSubarray = array.slice(midIndex + 1);
+    console.table([
+      { array, midIndex, midElement, leftSubarray, rightSubarray },
+    ]);
+    return {};
+  }
 
   function Node(value) {
     return { value, left: null, right: null };
   }
+
+  return {};
 }
--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 import Tree from "./BinarySearchTree.js";
+
+Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
